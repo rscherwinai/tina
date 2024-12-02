74b9060b3c5e9585955422b1a7e2e48e1e772f7b
rscherwinai <rscherwin.ai@gmail.com>
2024-12-01 20:09:19 -0500

    added multiple pages 3

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  speedInsights: {
+    enabled: false,
+  },
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+  },
+}
+
+module.exports = nextConfig 
